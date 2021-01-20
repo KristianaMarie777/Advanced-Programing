a70345c100f77ae9286fbc4f2f6b58f31c35b998
Kristiana Marie Montes <kristianamontes>
2021-01-20 18:07:09 -0500

    New work
    over 2 hour and 30mins of work

--- GAME1011/Lab 1/Platform.cpp
+++ GAME1011/Lab 1/Platform.cpp
@@ -1,8 +1,15 @@
 #include "Platform.h"
+#include <cctype>
 
 Platform::Platform()
 {
-	RetypePlatform();
+	cout << "\n\nWhat do you want to name the new Platform?\n\n";
+	SetPlatformName();
+
+	cout << "\nWhat is the name of the Manufactuer?\n\n";
+	SetManufactuer();
+
+	MainMenu();
 }
 
 void Platform::SetPlatformName()
@@ -17,11 +24,13 @@ void Platform::SetManufactuer()
 
 void Platform::CreateGame()
 {
+	games.push_back(new Game());
 }
 
 string Platform::PlatformList()
 {
-	return string();
+	string itemsInPlatform;
+	return itemsInPlatform;
 }
 
 void Platform::RetypePlatform()
@@ -29,15 +38,14 @@ void Platform::RetypePlatform()
 	char choice;
 	string nameChange;
 
+
 	do
 	{
-
-
 		cout << "What do you want to retype?\n\nA.Platform's Name\nB.Manufactuer's Name\nC.None of the above. Please go back.\n\n";
 
 		cin >> choice;
 
-		switch (choice)
+		switch (toupper(choice))
 		{
 		case 'A':
 
@@ -50,41 +58,146 @@ void Platform::RetypePlatform()
 
 				cin >> nameChange;
 
-				while (choice != '1' && choice != '2' && choice != '3')
+				while (toupper(choice) != 'A' && toupper(choice) != 'B' && toupper(choice) != 'C')
 				{
-					cout << "Are you sure you want to change Platform name from " << platformName << " to " << nameChange << "?";
-					cout << "\n\n1.Yes, change it\n2.No, I'd like to rewrite it\n3.I would like to leave it as " << platformName << endl << endl;
+					cout << "\nAre you sure you want to change Platform name from " << platformName << " to " << nameChange << "?";
+					cout << "\n\nA.Yes, change it\nB.No, I'd like to rewrite it\nC.I would like to leave it as " << platformName << endl << endl;
 
 					cin >> choice;
 
-					if (choice == '1')
+					if (toupper(choice) == 'A')
 					{
 						platformName = nameChange;
 
 						cout << "\n\nPlatform's name has been changed to " << platformName << endl << endl;
 					}
-					else if (choice == '3')
+					else if (toupper(choice) == 'B')
 					{
 						cout << "\n\nPlatform's name has lefted as " << platformName << endl << endl;
 					}
-					else if (choice != '2')
+					else if (toupper(choice) != 'C')
 					{
 						cout << "\n\nChoice does not exist. Try again\n";
 					}
 				}
 
-			} while (choice != '3' && choice != '1');
+			} while (toupper(choice) != 'C' && toupper(choice) != 'A');
 
 
 			break;
 		case 'B':
+
+			do
+			{
+				choice = '\n';
+
+				cout << "\n\nManufactuer's current name is: " << manufactuerName << endl << endl;
+				cout << "Changing name to ";
+
+				cin >> nameChange;
+
+				while (toupper(choice) != 'A' && toupper(choice) != 'B' && toupper(choice) != 'C')
+				{
+					cout << "\nAre you sure you want to change Manufactuer name from " << manufactuerName << " to " << nameChange << "?";
+					cout << "\n\nA.Yes, change it\nB.No, I'd like to rewrite it\nC.I would like to leave it as " << manufactuerName << endl << endl;
+
+					cin >> choice;
+
+					if (toupper(choice) == 'A')
+					{
+						manufactuerName = nameChange;
+
+						cout << "\n\nManufactuer's name has been changed to " << manufactuerName << endl << endl;
+					}
+					else if (toupper(choice) != 'C' && toupper(choice) == 'B')
+					{
+						cout << "\n\nChoice does not exist. Try again\n";
+					}
+				}
+
+			} while (toupper(choice) != 'C' && toupper(choice) != 'A');
 			break;
-		case ('C' && 'c'):
+
+
+		case 'C':
+			// making sure Default doesn't activate when C in entered
 			break;
 		default:
-			cout << "That is not one of the choices.\n";
+
+			cout << "\nThat is not one of the choices.\n";
 			break;
 		}
 
-	}while (choice != 'C' && choice != 'c');
+	}while (toupper(choice) != 'C');
+}
+
+void Platform::MainMenu()
+{
+	char choice;
+	int numChoice;
+
+	do
+	{
+		cout << "\n\n\nWelcome to " << platformName;
+		cout << "\n\nMade by " << manufactuerName << endl << endl;
+
+		if (games.empty())
+			cout << "It seems that you currently have no games. ";
+		else
+		{
+			cout << "Games";
+		}
+
+		cout << "Type:\n\nA.To create a Game\nB.To Exit Platform\nC.Change Platform or Manufactur's name";
+
+		if (!games.empty())
+			cout << "\nD. Enter The Game\n\n";
+
+		cout << "\nEnter choice here: ";
+		cin >> choice;
+
+		if (toupper(choice) == 'A')
+		{
+			CreateGame();
+		}
+		else if (toupper(choice) == 'C')
+		{
+			RetypePlatform();
+		}
+		else if (toupper(choice) == 'D')
+		{
+			if (!games.empty())
+			{
+				cout << "\n\nSorry. This option is not available at the moment.\nEnter A and Create a new game to activate choice later.";
+			}
+			else
+			{
+				do
+				{
+
+					cout << "\nWhich game do you want to Enter?\n\n";
+
+					for (int i = 0; i == games.size() - 1; i++)
+					{
+						cout << i << ". " << games[i]->GetGameName() << endl;
+					}
+					cout << games.size() << ". Go Back to Platform's Main Menu\n\n\nEnter choice here: ";
+
+					cin >> numChoice;
+
+					if (numChoice >= 0 && numChoice < games.size())
+						games[numChoice]->MainMenu();
+
+					else if (numChoice != games.size())
+						cout << "\n\nThat is not on of the choices.Please try again.\n";
+
+				} while (numChoice == games.size());
+			}
+		}
+		else if (toupper(choice) != 'B')
+		{
+			cout << "\nThat is not one if the following choices. Please try again.";
+		}
+
+	} while (toupper(choice)!='B');
 }
--- GAME1011/Lab 1/Platform.h
+++ GAME1011/Lab 1/Platform.h
@@ -15,7 +15,44 @@ private:
 	int scoreValue;
 
 public:
+	Achievement()
+	{
+		cout << "\n\nWhat do you like to name this Achievement\n\n";
+		cin >> achievementTitle;
 
+		cout << "\n\nWhat is this Achievement's Description?\n\n";
+		cin >> achievementDescription;
+
+		cout << "\n\nHow many points does this Achievement gives the player?\n\n";
+		cin >> scoreValue;
+
+		MainMenu();
+	}
+
+	string GetTitle()
+	{
+		return achievementTitle;
+	}
+
+	string AchievementFullDescription()
+	{
+		string list;
+
+		return list;
+	}
+
+	void RetypeAchievement()
+	{
+
+	}
+
+	void MainMenu()
+	{
+		char choice;
+		AchievementFullDescription();
+
+
+	}
 };
 
 class Game
@@ -25,7 +62,238 @@ private:
 	vector<Achievement*> achievement;
 
 public:
+	Game()
+	{
+		cout << "\nWhat Title do you want to give this Game?\n\n";
 
+		cin >> gameName;
+
+		cout << "\n\nWhat is the Publisher's Name?\n\n";
+
+		cin >> publisherName;
+
+		cout << "\n\nWhat is the Developer's Name?\n\n";
+
+		cin >> developerName;
+
+		MainMenu();
+
+	}
+
+	void CreateAchievement()
+	{
+		achievement.push_back(new Achievement());
+	}
+
+	string GetGameName()
+	{
+		return  gameName;
+	}
+
+	string GameList()
+	{
+		string list;
+
+		return list;
+	}
+
+	void RetypeGame()
+	{
+		char choice;
+		string nameChange;
+
+
+		do
+		{
+			cout << "What do you want to retype?\n\nA.Game's Name\nB.Publisher's Name\nC.Publisher's Name\nD.None of the above. Please go back.\n\n";
+
+			cin >> choice;
+
+			switch (toupper(choice))
+			{
+			case 'A':
+
+				do
+				{
+					choice = '\n';
+
+					cout << "\n\nGame's current name is: " << gameName << endl << endl;
+					cout << "Changing name to ";
+
+					cin >> nameChange;
+
+					while (toupper(choice) != 'A' && toupper(choice) != 'B' && toupper(choice) != 'C')
+					{
+						cout << "\nAre you sure you want to change Game name from " << gameName << " to " << nameChange << "?";
+						cout << "\n\nA.Yes, change it\nB.No, I'd like to rewrite it\nC.I would like to leave it as " << gameName << endl << endl;
+
+						cin >> choice;
+
+						if (toupper(choice) == 'A')
+						{
+							gameName = nameChange;
+
+							cout << "\n\nGame's name has been changed to " << gameName << endl << endl;
+						}
+						else if (toupper(choice) != 'C' && toupper(choice) == 'B')
+						{
+							cout << "\n\nChoice does not exist. Try again\n";
+						}
+					}
+
+				} while (toupper(choice) != 'C' && toupper(choice) != 'A');
+
+
+				break;
+			case 'B':
+
+				do
+				{
+					choice = '\n';
+
+					cout << "\n\nPublisher's current name is: " << publisherName << endl << endl;
+					cout << "Changing name to ";
+
+					cin >> nameChange;
+
+					while (toupper(choice) != 'A' && toupper(choice) != 'B' && toupper(choice) != 'C')
+					{
+						cout << "\nAre you sure you want to change Publisher name from " << publisherName << " to " << nameChange << "?";
+						cout << "\n\nA.Yes, change it\nB.No, I'd like to rewrite it\nC.I would like to leave it as " << publisherName << endl << endl;
+
+						cin >> choice;
+
+						if (toupper(choice) == 'A')
+						{
+							publisherName = nameChange;
+
+							cout << "\n\nPublisher's name has been changed to " << publisherName << endl << endl;
+						}
+						else if (toupper(choice) != 'C' && toupper(choice) != 'B')
+						{
+							cout << "\n\nChoice does not exist. Try again\n";
+						}
+					}
+
+				} while (toupper(choice) != 'C' && toupper(choice) != 'A');
+				break;
+
+			case 'C':
+
+				do
+				{
+					choice = '\n';
+
+					cout << "\n\nDeveloper's current name is: " << developerName << endl << endl;
+					cout << "Changing name to ";
+
+					cin >> nameChange;
+
+					while (toupper(choice) != 'A' && toupper(choice) != 'B' && toupper(choice) != 'C')
+					{
+						cout << "\nAre you sure you want to change Developer's name from " << developerName << " to " << nameChange << "?";
+						cout << "\n\nA.Yes, change it\nB.No, I'd like to rewrite it\nC.I would like to leave it as " << developerName << endl << endl;
+
+						cin >> choice;
+
+						if (toupper(choice) == 'A')
+						{
+							developerName = nameChange;
+
+							cout << "\n\nDeveloper's name has been changed to " << developerName << endl << endl;
+						}
+						else if (toupper(choice) != 'C' && toupper(choice) != 'B')
+						{
+							cout << "\n\nChoice does not exist. Try again\n";
+						}
+					}
+
+				} while (toupper(choice) != 'C' && toupper(choice) != 'A');
+
+
+				break;
+			case 'D':
+				// making sure Default doesn't activate when D in entered
+				break;
+			default:
+
+				cout << "\nThat is not one of the choices.\n";
+				break;
+			}
+
+		} while (toupper(choice) != 'C');
+	}
+	void MainMenu()
+	{
+		char choice;
+		int numChoice;
+
+		do
+		{
+			cout << "\n\n\nWelcome to " << gameName;
+			cout << "\n\npublished by " << publisherName << endl;
+			cout << "Developed by "<< developerName << endl;
+
+			if (achievement.empty())
+				cout << "It seems that you currently have no achievements. ";
+			else
+			{
+				cout << "In-Game Achievements";
+			}
+
+			cout << "Type:\n\nA.To create a Achievement\nB.To Exit Game\nC.Change Game, Publisher's name, and Developer's name";
+
+			if (!achievement.empty())
+				cout << "\nD. Look at Achievement's infomation/Edit Achievements\n\n";
+
+			cout << "\nEnter choice here: ";
+			cin >> choice;
+
+			if (toupper(choice) == 'A')
+			{
+				CreateAchievement();
+			}
+			else if (toupper(choice) == 'C')
+			{
+				RetypeGame();
+			}
+			else if (toupper(choice) == 'D')
+			{
+				if (!achievement.empty())
+				{
+					cout << "\n\nSorry. This option is not available at the moment.\nEnter A and Create a new Achievement to activate choice later.";
+				}
+				else
+				{
+					do
+					{
+
+						cout << "\nWhich Achievement do you want to know more about?\n\n";
+
+						for (int i = 0; i == achievement.size() - 1; i++)
+						{
+							cout << i << ". " << achievement[i]->GetTitle() << endl;
+						}
+						cout << achievement.size() << ". Go Back to Game's Main Menu\n\n\nEnter choice here: ";
+
+						cin >> numChoice;
+
+						if (numChoice >= 0 && numChoice < achievement.size())
+							achievement[numChoice]->MainMenu();
+
+						else if (numChoice != achievement.size())
+							cout << "\n\nThat is not on of the choices. Please try again.\n";
+
+					} while (numChoice == achievement.size());
+				}
+			}
+			else if (toupper(choice) != 'B')
+			{
+				cout << "\nThat is not one if the following choices. Please try again.";
+			}
+
+		} while (toupper(choice) != 'B');
+	}
 };
 
 class Platform
@@ -40,6 +308,7 @@ public:
 	void CreateGame();
 	string PlatformList();
 	void RetypePlatform();
+	void MainMenu();
 
 };
 
