c18dd582771355b4099f2beb814ada6db462875f
Kristiana Marie Montes <kristianamontes>
2021-01-20 13:35:00 -0500

    1:34 Jan 22 save
    Looking up switch

--- GAME1011/Lab 1/Platform.cpp
+++ GAME1011/Lab 1/Platform.cpp
@@ -15,6 +15,10 @@ void Platform::SetManufactuer()
 	cin >> manufactuerName;
 }
 
+void Platform::CreateGame()
+{
+}
+
 string Platform::PlatformList()
 {
 	return string();
@@ -23,7 +27,8 @@ string Platform::PlatformList()
 void Platform::RetypePlatform()
 {
 	char choice;
-	
+	string nameChange;
+
 	do
 	{
 
@@ -35,13 +40,49 @@ void Platform::RetypePlatform()
 		switch (choice)
 		{
 		case 'A':
+
+			do
+			{
+				choice = '\n';
+
+				cout << "\n\nPlatform's current name is: " << platformName << endl << endl;
+				cout << "Changing name to ";
+
+				cin >> nameChange;
+
+				while (choice != '1' && choice != '2' && choice != '3')
+				{
+					cout << "Are you sure you want to change Platform name from " << platformName << " to " << nameChange << "?";
+					cout << "\n\n1.Yes, change it\n2.No, I'd like to rewrite it\n3.I would like to leave it as " << platformName << endl << endl;
+
+					cin >> choice;
+
+					if (choice == '1')
+					{
+						platformName = nameChange;
+
+						cout << "\n\nPlatform's name has been changed to " << platformName << endl << endl;
+					}
+					else if (choice == '3')
+					{
+						cout << "\n\nPlatform's name has lefted as " << platformName << endl << endl;
+					}
+					else if (choice != '2')
+					{
+						cout << "\n\nChoice does not exist. Try again\n";
+					}
+				}
+
+			} while (choice != '3' && choice != '1');
+
+
 			break;
 		case 'B':
 			break;
-		case 'C':
-			cout << "hi";
+		case ('C' && 'c'):
 			break;
 		default:
+			cout << "That is not one of the choices.\n";
 			break;
 		}
 
