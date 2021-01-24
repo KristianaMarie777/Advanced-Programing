40235ce82ab35e27206465941802975a6f040fea
Kristiana Marie Montes <kristianamontes>
2021-01-24 00:01:32 -0500

    test

--- GAME1011/Lab 1/Lab 1.cpp
+++ GAME1011/Lab 1/Lab 1.cpp
@@ -7,6 +7,29 @@
 
 int main()
 {
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+    vector<Platform*> platforms;
+
+    platforms.push_back(new Platform());
+
+    return 0;
+=======
+    string names, numChoice;
+    int num, value;
+    Platform* platforms;
+    cout << "Hello user\n\n";
+
+    do
+    {
+
+        cout << "How Many Platforms do you want to create?: ";
+        cin >> numChoice;
+
+        system("CLS");
+
+        for (size_t i = 0; i < numChoice.size(); i++)
+=======
     string names, numChoice;
     int num, value;
     
@@ -21,11 +44,33 @@ int main()
         system("CLS");
 
         for (int i = 0; i < numChoice.size(); i++)
+>>>>>>> start
         {
             value = i;
             if (isalpha(numChoice[i]))
             {
                 cout << "That is not a number. Please type again.\n\n";
+<<<<<<< HEAD
+            }
+        }
+
+
+    } while (isalpha(numChoice[value]));
+
+    num = stoi(numChoice);
+
+    platforms = new Platform [num];
+    
+    for (int i = 0; i < num; i++)
+    {
+        cout << platforms[i].GetPlatformName();
+
+    }
+
+
+
+>>>>>>> Stashed changes
+=======
                 break;
             }
         }
@@ -50,5 +95,16 @@ int main()
         cout << (platforms+i)->GetPlatformDescription() << "\n\n\n";
     }
 
+>>>>>>> start
 }
 
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- GAME1011/Lab 1/Platform.h
+++ GAME1011/Lab 1/Platform.h
@@ -16,6 +16,39 @@ private:
 public:
 	void AddAchievementinfo(string a,int n)
 	{
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+		cout << "\n\nWhat do you like to name this Achievement\n\n";
+		cin >> achievementTitle;
+
+		cout << "\n\nWhat is this Achievement's Description?\n\n";
+		cin >> achievementDescription;
+
+		cout << "\n\nHow many points does this Achievement gives the player?\n\n";
+		cin >> scoreValue;
+
+		MainMenu();
+=======
+
+>>>>>>> Stashed changes
+	}
+
+	string GetTitle()
+	{
+		return achievementTitle;
+	}
+
+	string AchievementFullDescription()
+	{
+		string list;
+
+		return list;
+	}
+
+<<<<<<< Updated upstream
+	void RetypeAchievement()
+	{
+=======
 		string numChoice;
 		int num = 0;
 		achievementNum = n;
@@ -56,18 +89,32 @@ public:
 		} while (isalpha(numChoice[num]));
 
 		scoreValue = stoi(numChoice);
+>>>>>>> start
 
 	}
 
 	string GetAchievementDescription()
 	{
+<<<<<<< HEAD
+		char choice;
+		AchievementFullDescription();
+=======
+	void MainMenu()
+	{
+
+	}
+	string Achievementlist()
+	{
+		string list = "\n" + achievementTitle;
+>>>>>>> Stashed changes
+=======
 		string list = "Achievement "+to_string(achievementNum)+": "+ achievementTitle;
 
 		list += "\n" + achievementDescription;
 		list += "\nGet " + to_string(scoreValue) +"XP";
+>>>>>>> start
 
 
-		return list;
 	}
 
 };
@@ -82,6 +129,194 @@ private:
 public:
 	void AddGameInfo(string a, int n)
 	{
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+		cout << "\nWhat Title do you want to give this Game?\n\n";
+
+		cin >> gameName;
+
+		cout << "\n\nWhat is the Publisher's Name?\n\n";
+
+		cin >> publisherName;
+
+		cout << "\n\nWhat is the Developer's Name?\n\n";
+
+		cin >> developerName;
+
+		MainMenu();
+=======
+
+>>>>>>> Stashed changes
+
+	}
+
+	void CreateAchievement()
+	{
+		achievement.push_back(new Achievement());
+	}
+
+	string GetGameName()
+	{
+		return  gameName;
+	}
+
+	string GameList()
+	{
+		string list;
+
+		return list;
+	}
+
+<<<<<<< Updated upstream
+	void RetypeGame()
+	{
+		char choice;
+		string nameChange;
+
+
+		do
+		{
+			cout << "What do you want to retype?\n\nA.Game's Name\nB.Publisher's Name\nC.Publisher's Name\nD.None of the above. Please go back.\n\n";
+
+			cin >> choice;
+
+			switch (toupper(choice))
+			{
+			case 'A':
+
+				do
+				{
+					choice = '\n';
+
+					cout << "\n\nGame's current name is: " << gameName << endl << endl;
+					cout << "Changing name to ";
+
+					cin >> nameChange;
+
+					while (toupper(choice) != 'A' && toupper(choice) != 'B' && toupper(choice) != 'C')
+					{
+						cout << "\nAre you sure you want to change Game name from " << gameName << " to " << nameChange << "?";
+						cout << "\n\nA.Yes, change it\nB.No, I'd like to rewrite it\nC.I would like to leave it as " << gameName << endl << endl;
+
+						cin >> choice;
+
+						if (toupper(choice) == 'A')
+						{
+							gameName = nameChange;
+
+							cout << "\n\nGame's name has been changed to " << gameName << endl << endl;
+						}
+						else if (toupper(choice) != 'C' && toupper(choice) == 'B')
+						{
+							cout << "\n\nChoice does not exist. Try again\n";
+						}
+					}
+
+				} while (toupper(choice) != 'C' && toupper(choice) != 'A');
+
+
+				break;
+			case 'B':
+
+				do
+				{
+					choice = '\n';
+
+					cout << "\n\nPublisher's current name is: " << publisherName << endl << endl;
+					cout << "Changing name to ";
+
+					cin >> nameChange;
+
+					while (toupper(choice) != 'A' && toupper(choice) != 'B' && toupper(choice) != 'C')
+					{
+						cout << "\nAre you sure you want to change Publisher name from " << publisherName << " to " << nameChange << "?";
+						cout << "\n\nA.Yes, change it\nB.No, I'd like to rewrite it\nC.I would like to leave it as " << publisherName << endl << endl;
+
+						cin >> choice;
+
+						if (toupper(choice) == 'A')
+						{
+							publisherName = nameChange;
+
+							cout << "\n\nPublisher's name has been changed to " << publisherName << endl << endl;
+						}
+						else if (toupper(choice) != 'C' && toupper(choice) != 'B')
+						{
+							cout << "\n\nChoice does not exist. Try again\n";
+						}
+					}
+
+				} while (toupper(choice) != 'C' && toupper(choice) != 'A');
+				break;
+
+			case 'C':
+
+				do
+				{
+					choice = '\n';
+
+					cout << "\n\nDeveloper's current name is: " << developerName << endl << endl;
+					cout << "Changing name to ";
+
+					cin >> nameChange;
+
+					while (toupper(choice) != 'A' && toupper(choice) != 'B' && toupper(choice) != 'C')
+					{
+						cout << "\nAre you sure you want to change Developer's name from " << developerName << " to " << nameChange << "?";
+						cout << "\n\nA.Yes, change it\nB.No, I'd like to rewrite it\nC.I would like to leave it as " << developerName << endl << endl;
+
+						cin >> choice;
+
+						if (toupper(choice) == 'A')
+						{
+							developerName = nameChange;
+
+							cout << "\n\nDeveloper's name has been changed to " << developerName << endl << endl;
+						}
+						else if (toupper(choice) != 'C' && toupper(choice) != 'B')
+						{
+							cout << "\n\nChoice does not exist. Try again\n";
+						}
+					}
+
+				} while (toupper(choice) != 'C' && toupper(choice) != 'A');
+
+
+				break;
+			case 'D':
+				// making sure Default doesn't activate when D in entered
+				break;
+			default:
+
+				cout << "\nThat is not one of the choices.\n";
+				break;
+			}
+
+		} while (toupper(choice) != 'C');
+	}
+	void MainMenu()
+	{
+		char choice;
+		int numChoice;
+
+		do
+		{
+			cout << "\n\n\nWelcome to " << gameName;
+			cout << "\n\npublished by " << publisherName << endl;
+			cout << "Developed by "<< developerName << endl;
+
+			if (achievement.empty())
+				cout << "It seems that you currently have no achievements. ";
+			else
+			{
+				cout << "In-Game Achievements";
+			}
+
+			cout << "Type:\n\nA.To create a Achievement\nB.To Exit Game\nC.Change Game, Publisher's name, and Developer's name";
+
+			if (!achievement.empty())
+				cout << "\nD. Look at Achievement's infomation/Edit Achievements\n\n";
+=======
 		string numChoice;
 		int num = 0;
 		gameNum = n;
@@ -126,9 +361,32 @@ public:
 			}
 
 		} while (isalpha(numChoice[num]));
+>>>>>>> start
 
 		numOfAchievement = stoi(numChoice);
 
+<<<<<<< HEAD
+			if (toupper(choice) == 'A')
+			{
+				CreateAchievement();
+			}
+			else if (toupper(choice) == 'C')
+			{
+				RetypeGame();
+			}
+			else if (toupper(choice) == 'D')
+			{
+				if (!achievement.empty())
+				{
+					cout << "\n\nSorry. This option is not available at the moment.\nEnter A and Create a new Achievement to activate choice later.";
+				}
+				else
+				{
+					do
+					{
+
+						cout << "\nWhich Achievement do you want to know more about?\n\n";
+=======
 		achievement = new Achievement[stoi(numChoice)];
 
 		for (int i = 0;i < stoi(numChoice);i++)
@@ -137,6 +395,7 @@ public:
 			achievement[i].AddAchievementinfo(MenuText(a), i + 1);
 		}
 	}
+>>>>>>> start
 
 	string GetGameDescription()
 	{
@@ -163,18 +422,54 @@ public:
 		
 		text += "What is the name of Game " + to_string(gameNum) + "?: " + gameName;
 
+<<<<<<< HEAD
+						else if (numChoice != achievement.size())
+							cout << "\n\nThat is not on of the choices. Please try again.\n";
+
+					} while (numChoice == achievement.size());
+				}
+			}
+			else if (toupper(choice) != 'B')
+			{
+				cout << "\nThat is not one if the following choices. Please try again.";
+			}
+
+		} while (toupper(choice) != 'B');
+=======
+	void MainMenu()
+	{
+
+>>>>>>> Stashed changes
+=======
 		text += "\n\nWhat is the name of it's Publisher?: " + publisherName;
 
 		text += "\n\nWhat is the name of it's Developer?: " + developerName;
 		text += "\n\nHow Many Achievements are in " + gameName + "?: " + to_string(numOfAchievement);
 
 		return text;
+>>>>>>> start
 	}
 };
 
 class Platform
 {
 private:
+<<<<<<< HEAD
+	string platformName, manufactuerName;
+	vector<Game*> games;
+public:
+	Platform();
+	void SetPlatformName();
+	void SetManufactuer();
+	void CreateGame();
+	string PlatformList();
+	void MainMenu();
+<<<<<<< Updated upstream
+=======
+	string GetPlatformName();
+	void SetNames();
+>>>>>>> Stashed changes
+=======
 	string platformName, manufacturerName;
 	int numofGames, platformNum;
 	Game* games;
@@ -184,10 +479,9 @@ public:
 	string MenuText(string a);
 	string GetPlatformDescription();
 	string GetGameInfo(int n);
+>>>>>>> start
 
 };
-
-
 
 #endif // !1
 
