e639f3a724040deb27df2781cef44d2c2018c4db
Kristiana Marie Montes <kristianamontes>
2021-03-17 02:22:23 -0400

    Test 7

--- GAME1011/Lab 4/Lab 4.cpp
+++ GAME1011/Lab 4/Lab 4.cpp
@@ -7,7 +7,6 @@ int main()
 {
     std::cout << "Hello World!\n";
 }
-//here
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
