1770ef2eae4b22dc628bc03831bcfe37aa4abab1
Kristiana Marie Montes <kristianamontes>
2021-03-18 12:13:39 -0400

    added sort function and menu system

--- GAME1011/Lab 4/Lab 4.cpp
+++ GAME1011/Lab 4/Lab 4.cpp
@@ -4,10 +4,11 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
-bool palindrome(string word, int currentletter, int lettercheck)
+bool palindrome(string word, int currentletter, bool includeSpaces = false,int lettercheck = 0)
 {
 
     if (currentletter < 0 && lettercheck > word.size() - 1)
@@ -21,70 +22,61 @@ bool palindrome(string word, int currentletter, int lettercheck)
     }
     else if (word[currentletter] == word[lettercheck])
     {
-        return palindrome(word, currentletter - 1, lettercheck + 1);
+        return palindrome(word, currentletter - 1, includeSpaces,lettercheck + 1);
     }
 }
 
 
-void sort(int* numArray, int start, int end, int first ,int total)
+void sort(int* numArray, int start, int end)
 {
-    
-    cout << " start is " << start << " and end is " << end << endl;
-    
+    int mid = end;
+
+    cout << "start value is " << start << " end value is " << end << endl;
+
     for (int i = 0; i < 20; i++)
     {
         cout << numArray[i] << " ";
     }
-
     
-    cout << endl << endl;
+    cout << endl<< endl;
     
-    
-    if (start != total-1)
+    if (start < end)
     {
-        //cout << "change";
-        if (end == 0)
+        for(int begin = start + 1; begin <= mid;)
         {
-            sort(numArray, start, total - 1, start,total);
-        }
-
-        if (start > end)
-        {
-            if (numArray[end] < numArray[first])
+            if (numArray[mid] < numArray[begin])
             {
+                int beginChange = numArray[begin], midChange = numArray[mid];
+                numArray[begin] = midChange;
+                numArray[mid] = beginChange;
 
-                int startChange = numArray[first], endChange = numArray[end];
-
-                numArray[first] = endChange;
-                numArray[end] = startChange;
-
-                sort(numArray, start, end, first,total);
             }
 
-            sort(numArray, 0, end, 0,total);
+            if (numArray[begin] > numArray[start])
+            {
+                mid -= 1;
+            }
+            else
+            {
+                begin += 1;
+            }
+
         }
 
+        if (numArray[mid] < numArray[start])
+        {
+            int startChange = numArray[start], midChange = numArray[mid];
+            numArray[start] = midChange;
+            numArray[mid] = startChange;
+
+        }
+
+        cout << "\n\nsort(numArray, start = " << start << ", mid - 1 = " << mid - 1 << " );\n\n";
+        sort(numArray, start, mid - 1);
+        cout << "\n\nsort(numArray, mid + 1 = " << mid + 1 << ", end = " << end << ");\n\n";
+        sort(numArray, mid + 1, end);
         
-        if (numArray[end] < numArray[first] && numArray[end] < numArray[start])
-        {
-            
-            int startChange = numArray[start], endChange = numArray[end];
-
-            numArray[start] = endChange;
-            numArray[end] = startChange;
-            sort(numArray, start + 1, end, first, total);
-        }
-
-        if (numArray[start] > numArray[first])
-        {
-            sort(numArray, start, end - 1, first,total);
-        }
-        else
-        {
-            sort(numArray, start + 1, end, first,total);
-        }
     }
-
 }
 
 
@@ -118,29 +110,117 @@ int main()
     srand(time(NULL));
 
     int numArray[20];
-
-    for (int i = 0; i < 20; i++)
+    string choice, word, insideChoice;
+    bool reset = false;
+    do
     {
-        numArray[i] = rand() % 30 + 5;
-    }
+        cout << "Which function do you want to try out?\n\n\nA. Palindrome\nB. QuickSort\nC. Reverse\nD. None, Exist game\n\nEnter choice here: ";
 
-    cout << "Numbers in array are :\n";
-    for (int i = 0; i < 20; i++)
-    {
-        cout << numArray[i] <<" ";
-    }
-    
-    cout << endl << endl << endl;
+        getline(cin, choice);
+        
+        system("CLS");
+        if (isalpha(choice[0]))
+        {
+            switch (toupper(choice[0]))
+            {
+            case 'A':
+                cout << "Enter word here: ";
+                //cin >> word;
+                getline(cin,word);
+                
+                do
+                {
+                    if (reset)
+                    {
+                        cout << "Enter word here: " << word;
+                        cout << "\n\nNot one of the choices. Try again.\n";
+                    
+                    }
 
-    sort(numArray,0,19,0,20);
+                    cout << "\n\nInclude spaces?\nA. Yes\nB. No\n\nEnter choice here: ";
+                    getline(cin, insideChoice);
 
-    cout << "sorted Nums is:\n";
+                    if (!isalpha(insideChoice[0]))
+                    {
+                        reset = true;
+                        system("CLS");
+                    
+                    }
 
-    for (int i = 0; i < 20; i++)
-    {
-        cout << numArray[i] << " ";
-    }
-    
+                } while (toupper(insideChoice[0]) != 'A' || toupper(insideChoice[0]) != 'B' );
+
+                if (reset)
+                {
+                    reset = false;
+                }
+                
+                cout << "\n\nThe Word \"" << word << "\" is ";
+                if (!(palindrome(word, word.size() - 1)))
+                {
+                    cout << "not ";
+                }
+
+                cout << "a palindrome word.\n\n";
+
+                system("pause");
+
+                system("CLS");
+
+                break;
+            case 'B':
+                for (int i = 0; i < 20; i++)
+                {
+                    numArray[i] = rand() % 30;
+                }
+
+                cout << "Numbers in array are :\n";
+                for (int i = 0; i < 20; i++)
+                {
+                    cout << numArray[i] << " ";
+                }
+                
+                cout << endl << endl;
+
+                system("pause");
+
+                sort(numArray, 0, 19);
+
+                cout << "sorted Nums is:\n";
+
+                for (int i = 0; i < 20; i++)
+                {
+                    cout << numArray[i] << " ";
+                }
+
+                cout << endl << endl;
+                system("pause");
+
+                system("CLS");
+
+                break;
+            case 'C':
+                cout << "Enter word here: ";
+                getline(cin, word);
+                
+                cout << endl << endl << reverseWordS(word,word.size()-1) << endl << endl;
+
+                system("pause");
+
+                system("CLS");
+
+            case 'D':
+                cout << "Goodbye.\n\n";
+                break;
+            default:
+                cout << "That was not one of the choices. Try again.\n\n";
+                break;
+            }
+        }
+        else
+        {
+            cout << "That is not a letter. Try again.\n\n";
+        }
+    } while (toupper(choice[0]) != 'D');
 
     return 0;
 }
